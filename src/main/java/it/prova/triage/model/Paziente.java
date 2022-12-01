b39a09901a2fd486334c0aff1739c0b20966d4c2
MezzoDiego <diegomezzo90@gmail.com>
2022-12-01 20:20:43 +0100

    setup entity model

---
+++ src/main/java/it/prova/triage/model/Paziente.java
@@ -0,0 +1,91 @@
+package it.prova.triage.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paziente")
+public class Paziente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String cognome;
+	private String codiceFiscale;
+	private LocalDate dataRegistrazione;
+
+	@Enumerated(EnumType.STRING)
+	private StatoPaziente stato;
+
+	public Paziente() {
+		super();
+	}
+
+	public Paziente(Long id, String nome, String cognome, String codiceFiscale, LocalDate dataRegistrazione,
+			StatoPaziente stato) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+		this.stato = stato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public LocalDate getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(LocalDate dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public StatoPaziente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoPaziente stato) {
+		this.stato = stato;
+	}
+
+}
